5dcc17adad734f2da28f9e153b2329b0007bbdfc
Do Thu Thao <dothuthao1504@gmail.com>
2019-10-04 18:03:56 +0800

    Implement Header & set font-face

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,28 +6,36 @@ function Header() {
     <div className="Header">
       <div className="UpperHeader">
         <div className="LanguageSelector">
-          English
+          <div className="text">English</div>
+          <img alt="option" src="../../assets/images/icon_select.svg"/>
+        </div>
+        <div className="UpperHeaderlinks">
+          <div className="NavigationContainer">
+            <div className="NavigationLink">
+              <span>HOME</span>
+            </div>
+            <div className="NavigationLink">
+              <span>COURSE</span>
+            </div>
+            <div className="NavigationLink">
+              <span>CONTACT</span>
+            </div>
+          </div>
+          <div className="SignInDesktop">
+          <span>Sign in</span>
+          </div>
         </div>
       </div>
+
       <div className="LowerHeader">
         <div className="HamburgerMenu">
-          hamburger
+          <img alt="menu" src="../../assets/images/icon_hamburger.svg"/>
         </div>
-        <div className="NavigationLinks">
-          <div className="NavigationLink">
-            HOME
-          </div>
-          <div className="NavigationLink">
-            COURSE
-          </div>
-          <div className="NavigationLink">
-            CONTACT
-          </div>
-        </div>
-        <div className="SignIn">
-          Sign in
+        <div className="SignInMobile">
+        <span>Sign in</span>
         </div>
       </div>
+
     </div>
   );
 }
