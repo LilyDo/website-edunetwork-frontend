6afd8c6fff78554de175a1f7fd505fd4c7e74f1c
Do Thu Thao <dothuthao1504@gmail.com>
2019-10-07 00:49:43 +0800

    Implement Info Carousel

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,7 +1,28 @@
 import React from 'react';
 import './Content.scss';
+import Carousel from '../Carousel/Carousel';
 
 function Content() {
+  const infoCarouselData = [
+    {
+      icon: 'icon_plane.svg',
+      title: 'TẦM NHÌN',
+      subtitle: 'CHINH PHỤC NHỮNG TẦM CAO MỚI',
+      description:
+        'Trở thành nền tảng dẫn đầu trong lĩnh vực giáo dục và đạo tạo trực tuyến trên thế giới với cam kết mang đến những trải nghiệm học tập quý giá, nhắm giúp học viên khám phá bản thân, thay đổi tầm nhìn và đạt được những thành công trong cuộc sống.',
+      image_url: 'info_carousel_1.svg',
+    },
+    {
+      icon: 'icon_plane.svg',
+      title: 'SỨ MỆNH',
+      subtitle: '',
+      description: `Giúp cho tất cả thành viên không kể độ tuổi, ngành nghề, tôn giáo, giới tính đều có thể nâng cao tri thức, nâng cao thu nhập và thành công.
+
+        Chúng tôi cam kết không ngừng cải tiến mọi hoạt động, cập nhật thêm các khóa học, các chương trình để giúp thành viên ngày càng thành công hơn nữa.`,
+      image_url: 'info_carousel_1.svg',
+    },
+  ];
+
   return (
     <div>
       <div className="Video">
@@ -9,10 +30,40 @@ function Content() {
           width="560"
           height="315"
           src="https://www.youtube.com/embed/KRiSMXFpWIg"
-          frameborder="0"
+          frameBorder="0"
           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
-        ></iframe>
+          allowFullScreen
+        />
+      </div>
+      <div className="InfoCarousel">
+        <Carousel title="Carousel">
+          {infoCarouselData.map((infoCarouselItem, index) => (
+            <div className="InfoCardContainer" key={index}>
+              <div className="InfoText">
+                <div className="Icon">
+                  <img
+                    src={`../../assets/images/${infoCarouselItem.icon}`}
+                    alt="icon"
+                  ></img>
+                </div>
+                <div className="Title">{infoCarouselItem.title}</div>
+                <div className="Subtitle">
+                  {infoCarouselItem.subtitle}
+                </div>
+                <div className="Description">
+                  {infoCarouselItem.description}
+                </div>
+              </div>
+              <div className="Image">
+                <img
+                  // src={`../../assets/images/${infoCarouselItem.image_url}`}
+                  src="https://unsplash.it/550/372"
+                  alt="image"
+                />
+              </div>
+            </div>
+          ))}
+        </Carousel>
       </div>
     </div>
   );
