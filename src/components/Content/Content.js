5d9d9f71947a95892aaf2821d4542ee6346fb3eb
Do Thu Thao <dothuthao1504@gmail.com>
2019-10-08 02:58:20 +0800

    Use png for images instead of svg

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -10,7 +10,7 @@ function Content() {
       subtitle: 'CHINH PHỤC NHỮNG TẦM CAO MỚI',
       description:
         'Trở thành nền tảng dẫn đầu trong lĩnh vực giáo dục và đạo tạo trực tuyến trên thế giới với cam kết mang đến những trải nghiệm học tập quý giá, nhắm giúp học viên khám phá bản thân, thay đổi tầm nhìn và đạt được những thành công trong cuộc sống.',
-      image_url: 'info_carousel_1.svg',
+      image_url: 'info_carousel_1.png',
     },
     {
       icon: 'icon_link.svg',
@@ -19,7 +19,7 @@ function Content() {
       description: `Giúp cho tất cả thành viên không kể độ tuổi, ngành nghề, tôn giáo, giới tính đều có thể nâng cao tri thức, nâng cao thu nhập và thành công.
 
         Chúng tôi cam kết không ngừng cải tiến mọi hoạt động, cập nhật thêm các khóa học, các chương trình để giúp thành viên ngày càng thành công hơn nữa.`,
-      image_url: 'info_carousel_1.svg',
+      image_url: 'info_carousel_1.png',
     },
   ];
 
