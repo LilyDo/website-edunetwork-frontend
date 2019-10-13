c29d88b16082b6c4f5de9038da8559735b2364e1
phattv <phattv.dev@gmail.com>
2019-10-14 02:58:58 +0800

    Add Breadcrumb component

--- src/components/CoursePage/CoursePage.js
+++ src/components/CoursePage/CoursePage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import QuoteContainer from '../QuoteContainer/QuoteContainer';
 import CourseCarouselContainer from '../CourseCarouselContainer/CourseCarouselContainer';
+import Breadcrumb from '../Breadcrumb/Breadcrumb';
 
 function CoursePage() {
   return (
     <div>
+      <Breadcrumb />
       <QuoteContainer />
       <CourseCarouselContainer />
     </div>
