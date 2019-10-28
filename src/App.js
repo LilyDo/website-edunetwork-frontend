57a479174c04890d79520678eb77b64503fed744
phattv <phat@thecarousell.com>
2019-10-28 11:53:03 +0700

    Change App.js to React Component style

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
-import React from 'react';
+// vendor
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+// custom
 import './App.scss';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
@@ -8,40 +15,38 @@ import CourseDetailPage from './components/CourseDetailPage/CourseDetailPage';
 import OrderPage from './components/OrderPage/OrderPage';
 import ContactPage from './components/ContactPage/ContactPage';
 import LoginPopup from './components/LoginPopup/LoginPopup';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from 'react-router-dom';
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Header />
-        <div className="Content">
-          <Switch>
-            <Route exact path="/">
-              <HomePage />
-            </Route>
-            <Route exact path="/courses">
-              <CoursePage />
-            </Route>
-            <Route exact path="/courses/detail">
-              <CourseDetailPage />
-            </Route>
-            <Route exact path="/courses/order">
-              <OrderPage />
-            </Route>
-            <Route exact path="/contact">
-              <ContactPage />
-            </Route>
-          </Switch>
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Header />
+          <div className="Content">
+            <Switch>
+              <Route exact path="/">
+                <HomePage />
+              </Route>
+              <Route exact path="/courses">
+                <CoursePage />
+              </Route>
+              <Route exact path="/courses/detail">
+                <CourseDetailPage />
+              </Route>
+              <Route exact path="/courses/order">
+                <OrderPage />
+              </Route>
+              <Route exact path="/contact">
+                <ContactPage />
+              </Route>
+            </Switch>
+          </div>
+          <Footer />
+          <LoginPopup />
         </div>
-        <Footer />
-        <LoginPopup />
-      </div>
-    </Router>
-  );
+      </Router >
+    );
+  }
 }
 
 export default App;
