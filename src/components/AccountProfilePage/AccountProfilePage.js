3fbeb9948baac0ee086058e847e430f82c6bbb2d
Do Thu Thao <dothuthao1504@gmail.com>
2019-10-30 15:39:46 +0800

    Implement ChangeProfile component

--- src/components/AccountProfilePage/AccountProfilePage.js
+++ src/components/AccountProfilePage/AccountProfilePage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import AccountBreadcrumb from '../AccountBreadcrumb/AccountBreadcrumb';
 import MyProfile from '../MyProfile/MyProfile';
+import ChangeProfile from '../ChangeProfile/ChangeProfile';
 
 function AccountProfilePage() {
   return (
     <div>
       <AccountBreadcrumb />
       <MyProfile />
+      <ChangeProfile />
     </div>
   );
 }
