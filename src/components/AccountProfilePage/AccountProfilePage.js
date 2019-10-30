06121aa9335fb9c2d7b078b681e1f950dbe04f56
Do Thu Thao <dothuthao1504@gmail.com>
2019-10-30 20:53:58 +0800

    Implement MyWallet, MyWallet_Withdraw & WithdrawNotification

--- src/components/AccountProfilePage/AccountProfilePage.js
+++ src/components/AccountProfilePage/AccountProfilePage.js
@@ -2,6 +2,9 @@ import React from 'react';
 import AccountBreadcrumb from '../AccountBreadcrumb/AccountBreadcrumb';
 import MyProfile from '../MyProfile/MyProfile';
 import ChangeProfile from '../ChangeProfile/ChangeProfile';
+import MyWallet from '../MyWallet/MyWallet';
+import MyWallet_Withdraw from '../MyWallet_Withdraw/MyWallet_Withdraw';
+import WithdrawNotification from '../WithdrawNotification/WithdrawNotification';
 
 function AccountProfilePage() {
   return (
@@ -9,6 +12,9 @@ function AccountProfilePage() {
       <AccountBreadcrumb />
       <MyProfile />
       <ChangeProfile />
+      <MyWallet />
+      <MyWallet_Withdraw />
+      <WithdrawNotification />
     </div>
   );
 }
