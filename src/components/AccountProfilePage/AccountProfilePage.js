031c15a765997daf5bee858127b273c92ed3731a
Do Thu Thao <dothuthao1504@gmail.com>
2019-10-30 12:46:07 +0800

    Implement AccountProfilePage

--- src/components/AccountProfilePage/AccountProfilePage.js
+++ src/components/AccountProfilePage/AccountProfilePage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import AccountBreadcrumb from '../AccountBreadcrumb/AccountBreadcrumb';
+import MyProfile from '../MyProfile/MyProfile';
 
 function AccountProfilePage() {
   return (
     <div>
       <AccountBreadcrumb />
+      <MyProfile />
     </div>
   );
 }
