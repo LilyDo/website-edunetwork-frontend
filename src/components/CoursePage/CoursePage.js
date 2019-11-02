421dec6021a480d489b768999717512d4666850d
phattv <phattv.dev@gmail.com>
2019-11-02 20:04:57 +0800

    Implement Breadcrumb componnet

--- src/components/CoursePage/CoursePage.js
+++ src/components/CoursePage/CoursePage.js
@@ -2,11 +2,17 @@ import React from 'react';
 import QuoteContainer from '../QuoteContainer/QuoteContainer';
 import CourseCarouselContainer from '../CourseCarouselContainer/CourseCarouselContainer';
 import Breadcrumb from '../Breadcrumb/Breadcrumb';
+import { routes } from '../../constants';
 
 function CoursePage() {
   return (
     <div>
-      <Breadcrumb />
+      <Breadcrumb
+        data={[
+          { link: routes.home, text: 'HOME' },
+          { link: routes.courses, text: 'COURSES' },
+        ]}
+      />
       <QuoteContainer />
       <CourseCarouselContainer />
     </div>
--- src/components/OrderPage/OrderPage.js
+++ src/components/OrderPage/OrderPage.js
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { withRouter } from 'react-router';
 import Breadcrumb from '../Breadcrumb/Breadcrumb';
 import OrderInfo from '../OrderInfo/OrderInfo';
+import { routes } from '../../constants';
 
-function OrderPage() {
-  return (
-    <div>
-      <Breadcrumb />
-      <OrderInfo />
-    </div>
-  );
+class OrderPage extends Component {
+  render() {
+    return (
+      <div>
+        <Breadcrumb
+          data={[
+            { link: routes.home, text: 'HOME' },
+            { link: routes.courses, text: 'COURSES' },
+            {
+              link: routes.courseDetail.replace(
+                ':id',
+                this.props.match.id,
+              ),
+              text: 'COURSE DETAILS',
+            },
+            {
+              link: routes.courseOrder.replace(
+                ':id',
+                this.props.match.id,
+              ),
+              text: 'ORDER DETAILS',
+            },
+          ]}
+        />
+        <OrderInfo />
+      </div>
+    );
+  }
 }
 
-export default OrderPage;
+export default withRouter(OrderPage);
