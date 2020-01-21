01bf56a60bd13d7d42afe52a744a54880522181f
Minh Tri GKC <minhtri@giakiemcoder.com>
2020-01-21 14:45:03 +0700

    update translated text

---
+++ src/services/lang.js
@@ -0,0 +1,104 @@
+export const translatedText = {
+  course: {
+    en: 'Course',
+    vi: 'Khoá học',
+  },
+  home: {
+    en: 'Home',
+    vi: 'Trang chủ',
+  },
+  contact: {
+    en: 'Contact',
+    vi: 'Liên hệ',
+  },
+  login: {
+    en: 'Login',
+    vi: 'Đăng nhập',
+  },
+  register: {
+    en: 'Register',
+    vi: 'Đăng ký',
+  },
+  signin: {
+    en: 'Sign in',
+    vi: 'Đăng nhập',
+  },
+  vision: {
+    en: 'VISION',
+    vi: 'TẦM NHÌN',
+  },
+  mission: {
+    en: 'MISSION',
+    vi: 'SỨ MỆNH',
+  },
+  corevalues: {
+    en: 'CORE VALUES',
+    vi: 'GIÁ TRỊ CỐT LÕI',
+  },
+  soartogreater: {
+    en: 'SOAR TO GREATER HEIGHTS',
+    vi: 'CHINH PHỤC NHỮNG TẦM CAO MỚI',
+  },
+  visiondetail: {
+    en:
+      'To bring education and networking easily available for everyone making our portal the most comprehensive and resourceful platform in the world at anytime and anywhere.',
+    vi:
+      'Trở thành nền tảng dẫn đầu trong lĩnh vực giáo dục và đào tạo trực tuyến trên thế giới với cam kết mang đến những trải nghiệm học tập quý giá, nhằm giúp học viên khám phá bản thân, thay đổi tầm nhìn và đạt được những thành công trong cuộc sống.',
+  },
+  missiondetail: {
+    en:
+      'Helping all members regardless of age, industry, religion and gender to enhance their knowledge, increase income and succeed. We are committed to constantly improving all activities, updating more courses and programs to help our members become more and more successful.',
+    vi:
+      'Giúp cho tất cả thành viên không kể độ tuổi, ngành nghề, tôn giáo, giới tính đều có thể nâng cao tri thức, nâng cao thu nhập và thành công.Chúng tôi cam kết không ngừng cải tiến mọi hoạt động, cập nhật thêm các khoá học, các chương trình để giúp cho thành viên ngày càng thành công hơn nữa.',
+  },
+  corevaluedetail: {
+    en:
+      'We put customer satisfaction as the core value. We are committed to creating a friendly, professional working and learning environment where every member has the opportunity to grow. We operate on transparency, ethics and efficiency. We make continuous efforts to improve, perfect and develop. We contribute positively to the community.',
+    vi:
+      'Chúng tôi đặt sự hài lòng của khách hàng là giá trị cốt lõi. Chúng tôi cam kết xây dựng môi trường học tập và làm việc chuyên nghiệp thân thiện, nơi mọi thành viên đều có cơ hội phát triển. Chúng tôi hoạt động dựa trên tính minh bạch, đạo đức và hiệu quả. Chúc tôi nổ lực không ngừng để cải tiến, hoàn thiện và phát triển. Chúng tôi đóng góp tích cực cho cộng đồng.',
+  },
+  slogan: {
+    en:
+      "BUILDING A COMMUNITY TO ENPOWER AND ENRICH EVERY INDIVIDUAL'S LIVES THROUGH EDUCATION AND NETWORKING.",
+    vi:
+      'CHÚNG TÔI MUỐN XÂY DỰNG MỘT CỘNG ĐỒNG THAY ĐỔI VỀ TRI THỨC VÀ THAY ĐỔI VỀ THU NHẬP',
+  },
+  ourcourses: {
+    en: 'OUR COURSES',
+    vi: 'KHÓA HỌC CỦA CHÚNG TÔI',
+  },
+  coursenote: {
+    en:
+      'Note: The price is included tax, you do not have to pay any more extra cost.',
+    vi:
+      'Lưu ý: Giá đã bao gồm thuế phí, bạn không phải trả thêm bất kỳ chi phí nào.',
+  },
+  coursename1: {
+    en: 'The Art Of Positive Mindset',
+    vi: 'Làm chủ tư duy thịnh vượng',
+  },
+  coursename2: {
+    en: 'Sales Power Up',
+    vi: 'Nghệ thuật bán hàng đỉnh cao',
+  },
+  coursename3: {
+    en: 'Marketing Hacks',
+    vi: 'Phù thủy Marketing',
+  },
+  coursename4: {
+    en: 'Master In Leadership',
+    vi: 'Bậc thầy về nghệ thuật lãnh đạo',
+  },
+  coursename5: {
+    en: 'Harness The Power Within',
+    vi: 'Khai thác sức mạnh tiềm ẩn bên trong bạn',
+  },
+  seedetail: {
+    en: 'SEE DETAIL',
+    vi: 'XEM CHI TIẾT',
+  },
+  connectwithus: {
+    en: 'CONNECT WITH US',
+    vi: 'KẾT NỐI VỚI CHÚNG TÔI',
+  },
+};
